f1a3d944322ec21842a1fb9f483811a1280d15a9
e <jannatul0040@gmail.com>
2023-08-18 11:36:47 +0600

    triangle calculation

---
+++ scripts/area-calculator.js
@@ -0,0 +1,22 @@
+function calculateTriangleArea(){
+    // get triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText)
+    console.log(base);
+
+    // get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area)
+
+    // show triangle area 
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+
+    addToCalculationEntry('Triangle', area);
+}
