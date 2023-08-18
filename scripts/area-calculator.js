c5e1b4c8010858a52fb641633d163902e7b3db3b
e <jannatul0040@gmail.com>
2023-08-18 14:22:07 +0600

    rectangle calculation

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -20,3 +20,35 @@ function calculateTriangleArea(){
 
     addToCalculationEntry('Triangle', area);
 }
+
+function calculateRectangleArea(){
+    // get rectangle width
+    const widthField = document.getElementById('rectangle-width')
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    // get rectangle length
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    // validate input: width and length
+    if(isNaN(width) || isNaN(length)){
+        alert('Please insert a number');
+        return;
+    }
+
+    // calculate area
+    const area = width * length;
+
+    // show rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+    addToCalculationEntry('Rectangle', area);
+}
+
+
+// reusable function --> reduce duplicate code
